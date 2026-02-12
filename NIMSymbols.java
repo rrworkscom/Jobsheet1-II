fa1b77de29c8a3ecc56f1731ede73d3329aa9fa2
rrworkscom <rrworks010@gmail.com>
2026-02-12 13:18:31 +0700

    2 Last Digits Converter

---
+++ NIMSymbols.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NIMSymbols {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input last 2 digits of your NIM: ");
+        int n = input.nextInt();
+
+        if (n < 10) {
+            n += 10;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 10 || i == 15) {
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.print("#");
+            }
+           else if (i % 2 == 0) {
+            System.out.print(i);
+           }
+           else {
+            System.out.print("*");
+           }
+        }
+    }
+}
